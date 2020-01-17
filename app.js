18401540ae69bcd6e0826eeacfb70facc55c2c49
Nathan Martel <nathanmartel@gmail.com>
2020-01-16 18:02:50 -0800

    Pair programming updates

--- app.js
+++ app.js
@@ -1,96 +1,66 @@
-//import checkResult from './checkResult.js';
-//import randomThrow from './randomThrow.js';
-import { randomThrow, computerThrowValue, checkResult } from './randomThrow.js';
+import { randomThrow, checkResult } from './randomThrow.js';
+
 const button = document.getElementById('shoot-button');
 const resetButton = document.getElementById('reset-button');
 const gameResults = document.getElementById('game-results');
-//onsole.log(gameResults);
-//const gameResults = document.getElementById('user-outcome');
-//const gameResults = document.getElementById('draw-outcome');
 const winsTotal = document.getElementById('win-total');
 const lossTotal = document.getElementById('loss-total');
 const drawTotal = document.getElementById('draw-total');
 
 
-  //State declaration
+//State declaration
 let wins = 0;
 let loses = 0;
 let draws = 0;
 
-  //Adds button to invoke defined functionality 
-button.addEventListener("click", () => {
-    console.log("click heard!")
+function playGame() {
+  console.log("click heard!")
 
   //this assigns the users selection to a var 
     const userChoice = document.querySelector('input:checked');
-    console.log (userChoice);
 
   //this assigns the value of the user's selections to a var  
     const userMove = userChoice.value;
-    console.log(userMove)
 
   //assigns the value of randomThrow to var
-    const computerMove = (randomThrow())
-     console.log(computerMove);
+    const computerMove = (randomThrow());
   
   //this assigns the games's outcome to a var by eval userMove and computerMove
     const outcome = checkResult(userMove, computerMove);
-    console.log(outcome);
-    //return outcome;
   
 // function to toggle visibility of result announcement
 
-let announceResult = () => {
-  
-        if (outcome === 'win') {
-            gameResults.textContent = "You Win!";
-            gameResults.style.visibility = 'visible';
-            return;
-        }  
-        if (outcome === 'lose') {
-            gameResults.textContent = "You Lose...";
-            gameResults.style.visibility = 'visible';
-            return;
-        }  
-        else {
-            gameResults.textContent = "It's a draw, try again!";
-            gameResults.style.visibility = 'visible';
-            return;
-        }
-    
+    let announceResult = () => {
+        if (outcome === 'win') gameResults.textContent = 'You Win!';
+        else if (outcome === 'lose') gameResults.textContent = 'You Lose...';
+        else gameResults.textContent = 'It\'s a draw, try again!';
+        gameResults.style.display = 'inherit';
     }; 
 
     if (outcome === 'win') {
         wins++;
         winsTotal.textContent = wins;
-        //gameResults.textContent = "You Win";
-        announceResult();   
-    }
-
-    if (outcome === 'lose') {
+    } else if (outcome === 'lose') {
         loses++;
         lossTotal.textContent = loses;
-        //gameResults.textContent = "You lose";
-        announceResult(); 
-    }
-
-    if (outcome === 'draw') {
+    } else {
         draws++;
         drawTotal.textContent = draws;
-        //gameResults.textContent = "It's a draw";
-        announceResult();
     }
-});
+    announceResult();
+}
 
-resetButton.addEventListener("click", () => {
-    console.log("click heard!")
+//Adds button to invoke defined functionality 
+button.addEventListener('click', playGame);
+
+resetButton.addEventListener('click', () => {
+    console.log('click heard!');
     wins = 0;
     loses = 0;
     draws = 0;
     winsTotal.textContent = wins;
     lossTotal.textContent = loses;
     drawTotal.textContent = draws;
-    gameResults.style.visibily = "hidden";
-
+    gameResults.style.display = 'none';
 });
 
--- randomThrow.js
+++ randomThrow.js
@@ -1,13 +1,7 @@
-//import checkResult from './checkResult.js'
+export { randomThrow, checkResult };
 
-//export default 
-
-function randomThrow(){
-    const computerThrow = Math.floor(Math.random() * 3);
-    return computerThrowValue(computerThrow);
-}
-
-function computerThrowValue(num) {
+function randomThrow() {
+    const num = Math.floor(Math.random() * 3);
     if (num === 0) {
         return 'rock';
     }
@@ -17,33 +11,12 @@ function computerThrowValue(num) {
     if (num === 2) {
         return 'scissors';
     }
-};
-
-function checkResult(player, computer) {
-    
-    if (player === computer) {
-        return 'draw';
-    };
-    if (player === 'rock' && computer == 'scissors') {
-        return 'win';
-    }; 
-    if (player === 'paper' && computer === 'rock') {
-        return 'win';
-    };
-    if (player === 'scissors' && computer == 'paper') {
-        return 'win'; 
-    } else {
-        return 'lose';
-    }
 }
 
-
-export {
-    randomThrow,
-    computerThrowValue,
-    checkResult
+function checkResult(player, computer) {  
+    if (player === computer) return 'draw';
+    if (player === 'rock' && computer === 'scissors') return 'win';
+    if (player === 'paper' && computer === 'rock') return 'win';
+    if (player === 'scissors' && computer === 'paper') return 'win'; 
+    else return 'lose';
 }
-
-
-
-
